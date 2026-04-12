7a68c3b7fddd06ffebdc5f46b25721783ff71469
Jakub Zárybnický <jakub@zarybnicky.com>
2026-04-12 23:01:08 +0200

    Fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
-// https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
-import eslintConfigPrettier from "eslint-config-prettier/flat";
+const eslintConfigPrettier = require("eslint-config-prettier/flat");
 
 module.exports = defineConfig([
   expoConfig,
